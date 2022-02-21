0606a73eb7ca43f57f75994714ebdf710d3a9e79
sqooid <lucasliu7@hotmail.com>
2022-02-21 15:38:57 +1100

    first commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<slot />
+
+<style>
+  :global(body) {
+    font-family: var(--svui-font);
+    background-color: var(--svui-background);
+    color: var(--svui-text);
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  import { TextInput, Button, Select } from 'svui'
+  const states = [
+    'sydney',
+    'melbourne',
+    'brisbane',
+    'perth',
+    'adelaide',
+    'canberra',
+    'hobart',
+    'darwin',
+  ]
+  const options = states.map((state) => {
+    return {
+      value: state,
+      title: state.charAt(0).toUpperCase() + state.slice(1),
+    }
+  })
+
+  let email = ''
+  let emailValid = true
+  let state = ''
+  let stateValid = true
+
+  const onSubscribe = () => {
+    emailValid = /^.+@.+\..+/.test(email) && !!email.length
+    stateValid = !!state.length
+  }
+</script>
+
+<svelte:head><title>Fuel Notify</title></svelte:head>
+<div id="index">
+  <TextInput placeholder="Email" bind:value={email} invalid={!emailValid} />
+  <Select {options} bind:value={state} />
+  <Button primary label="Subscribe" on:click={onSubscribe} />
+</div>
+
+<style>
+  #index {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+  }
+</style>
